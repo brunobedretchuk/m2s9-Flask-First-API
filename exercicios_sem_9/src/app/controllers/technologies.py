a4c84b65cbdfdfebe788de05152abaf37dd68e82
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-08-01 17:23:57 -0300

    v1

---
+++ exercicios_sem_9/src/app/controllers/technologies.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+technology = Blueprint('technology', __name__, url_prefix="/technology")
+
+@technology.route('/' , methods = ['GET'])
+def list_all_technologies():
+    return {"data": ["Java" , "Javascript"]}
+
+
+@technology.route("/" , methods = ['POST'])
+def add_new_technology():
+    return {"data": ["Java", "Javascript", "Python"]}
